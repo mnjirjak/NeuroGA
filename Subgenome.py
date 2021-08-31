74e431754a324aa56c18ce048db91cca9caa1bf1
Marko Njirjak <mnjirjak@riteh.hr>
2021-08-31 15:56:58 +0200

    Add Subgenome interface.

---
+++ Subgenome.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+
+class Subgenome(ABC):
+    """An interface used to define a Subgenome and its methods.
+
+    Each subgenome must implement it.
+    """
+    @abstractmethod
+    def random(self):
+        """Randomly initialize values in subgenome."""
+        pass
+
+    @abstractmethod
+    def recombination(self, partner):
+        """Perform recombination between current subgenome and `partner`.
+
+        :param Subgenome partner: A subgenome we want to combine with.
+        :return SubGenome
+        """
+        pass
+
+    @abstractmethod
+    def mutate(self):
+        """Mutate current subgenome."""
+        pass
