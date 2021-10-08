9959c26f1004b898906c1288db911bd1bf8fe720
Marko Njirjak <mnjirjak@riteh.hr>
2021-10-08 14:01:32 +0200

    Change recombination value transfer mechanism in RealNumber.

--- subgenomes/real_number.py
+++ subgenomes/real_number.py
@@ -4,11 +4,10 @@ import numpy as np
 
 class RealNumber(Subgenome):
     """A real number bound by min and max values."""
-    def __init__(self, min_value=0.0, max_value=1.0, value=0.0, mutation_probability=None):
+    def __init__(self, min_value=0.0, max_value=1.0, mutation_probability=None):
         """
         :param float min_value: Minimum value of the number.
         :param float max_value: Maximum value of the number.
-        :param float value: Current value of the number.
         :param float mutation_probability: Local mutation probability. If not specified, global mutation probability
                                            will be used.
         """
@@ -17,7 +16,7 @@ class RealNumber(Subgenome):
         self.__min_value = min_value
         self.__max_value = max_value
 
-        self.real_number = value
+        self.real_number = 0.0
 
     def randomize(self):
         """Randomly assign value in [self.__min_value, self.__max_value) range."""
@@ -32,12 +31,17 @@ class RealNumber(Subgenome):
         :param RealNumber partner
         :return: RealNumber
         """
-        return RealNumber(
+        # Create new child object.
+        child = RealNumber(
             min_value=self.__min_value,
             max_value=self.__max_value,
-            value=(self.real_number + partner.real_number) / 2,
             mutation_probability=self.mutation_probability
         )
+
+        # Set child value.
+        child.real_number = (self.real_number + partner.real_number) / 2
+
+        return child
 
     def mutate(self):
         """Perform mutation, introduce a slight variation.
