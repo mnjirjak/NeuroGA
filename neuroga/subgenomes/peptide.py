c4161b9f961eb7608f66cdccfe4badc274be4acc
mnjirjak <mnjirjak@riteh.hr>
2021-12-27 13:56:50 +0100

    Finish extract_important_groups function.

--- neuroga/subgenomes/peptide.py
+++ neuroga/subgenomes/peptide.py
@@ -8,8 +8,8 @@ class ModifiedPeptide(Subgenome):
     def __init__(self,
                  initial_peptide="",
                  control_mask=None,
-                 min_length=5,
-                 max_length=50,
+                 initial_min_length=5,
+                 initial_max_length=50,
                  amino_acids=None,
                  mutation_probability=None):
         """
@@ -27,23 +27,20 @@ class ModifiedPeptide(Subgenome):
         self.__initial_peptide = initial_peptide
         self.__control_mask = control_mask
 
-        self.__min_length = min_length
-        self.__max_length = max_length
+        self.__initial_min_length = initial_min_length
+        self.__initial_max_length = initial_max_length
 
         self.__amino_acids = amino_acids
 
         self.__important_groups = []
 
         if control_mask is not None:
-            self.__important_groups, self.mandatory_length = self.extract_important_groups(initial_peptide, control_mask)
+            self.__important_groups, self.mandatory_length = \
+                self.extract_important_groups(initial_peptide, control_mask)
 
         # Min i max length su zapravo ovisni o ovoj mandatory_length
 
-
-
-
-        self.__peptide_amino_acids = []
-        self.__peptide_important_groups = {}
+        self.__peptide = []
 
     def randomize(self):
         """Ovdje možemo imati dva pristupa:
@@ -52,29 +49,25 @@ class ModifiedPeptide(Subgenome):
         """
 
         num_amino_acids = np.random.randint(
-            low=self.__min_length - self.mandatory_length,
-            high=self.__max_length - self.mandatory_length + 1
+            low=self.__initial_min_length - self.mandatory_length,
+            high=self.__initial_max_length - self.mandatory_length + 1
         )
 
+        #Ovdje možeš maknuti low i ostaviti samo high i bez navođenja
         amino_acid_indices = np.random.randint(
             low=0,
             high=len(self.__amino_acids),
             size=num_amino_acids
         )
 
-        self.__peptide_amino_acids = [self.__amino_acids[amino_acid_index] for amino_acid_index in amino_acid_indices]
+        self.__peptide = [self.__amino_acids[amino_acid_index] for amino_acid_index in amino_acid_indices]
 
-        for group in self.__important_groups:
-            self.__peptide_important_groups[group] = []
+        # Ovdje će biti dict za important groups
 
-            for _ in range(self.__important_groups[group]):
-                insert_index = np.random.randint(
-                    low=0,
-                    high=(len(self.__peptide_amino_acids) + 1)
-                )
+        self.__peptide += \
+            [group for group in self.__important_groups for _ in range(self.__important_groups[group])]
 
-                self.__peptide_important_groups[group].append(insert_index)
-
+        self.__peptide = list(np.random.permutation(self.__peptide))
 
     def recombination(self, partner):
         """Combine this individual with `partner` using arithmetic mean.
@@ -85,128 +78,61 @@ class ModifiedPeptide(Subgenome):
         :param RealNumber partner
         :return: RealNumber
         """
+        crossover_index_parent_1 = len(self.__peptide) // 2
+        crossover_index_parent_2 = len(partner.get_peptide()) // 2
 
-        child_amino_acids_length = len(self.__peptide_amino_acids) + len(partner.get_peptide_amino_acids())
+        child_peptide = self.__peptide[:crossover_index_parent_1] + partner.get_peptide()[crossover_index_parent_2:]
 
-        crossover_index_parent_1 = np.random.randint(low=0, high=len(self.__peptide_amino_acids))
-        crossover_index_parent_2 = np.random.randint(low=0, high=len(partner.get_peptide_amino_acids()))
+        # Što ako su important grupe jedna aminokiselina, onda imamo problem
 
-        child_amino_acids = self.__peptide_amino_acids[:crossover_index_parent_1] + \
-                            partner.get_peptide_amino_acids()[crossover_index_parent_2:]
-
-
-# Dulčjina peptida i gdje je imp. group
-        child_important_groups = {}
-
+        # Dodaj grupe koje fale
+        # možda na random grupe staviti
         for group in self.__important_groups:
-            child_important_groups[group] = []
+            count = child_peptide.count(group)
 
-            for i in range(self.__important_groups[group]):
+            for i in range(self.__important_groups[group] - count):
                 if np.random.rand() < 0.5:
-                    insert_index = self.__peptide_important_groups[group][i]
+                    r = self.__peptide
                 else:
-                    insert_index = partner.get_peptide_important_groups()[group][i]
+                    r = partner.get_peptide()
 
-                child_important_groups[group].append(insert_index)
+                indices = [i for i in range(len(r)) if r[i] == group]
 
+                random_index = indices[np.random.randint(low=0, high=len(indices))]
+                random_index_modified = int(((random_index + 1) / len(r)) * len(child_peptide))
 
+                # Napraviti da se može i na kraj insertati
+                child_peptide.insert(random_index_modified, group)
 
+        # Makni grupe koje su viška
+        # možda na random grupe staviti
+        for group in self.__important_groups:
+            count = child_peptide.count(group)
 
+            # Ovdje ig_dict[group] nes mije biti veći od counta, čini mi se.
+            for i in range(count - self.__important_groups[group]):
+                indices = [i for i in range(len(child_peptide)) if child_peptide[i] == group]
 
+                random_index = indices[np.random.randint(low=0, high=len(indices))]
 
-
-        first_parent = ''.join(self.peptide)
-        second_parent = ''.join(partner.peptide)
-
-        child_length = (len(first_parent) + len(second_parent)) // 2 # Ovdje možda ne aritmetička sredina
-
-        random_length_first_parent = np.random.randint(low=0, high=child_length + 1)
-        second_parent_length = child_length - random_length_first_parent
-
-        # Mislim da ovo može i bez petlje
-        while True:
-            if random_length_first_parent > len(first_parent):
-                random_length_first_parent -= 1
-                second_parent_length += 1
-            elif second_parent_length > len(second_parent):
-                random_length_first_parent += 1
-                second_parent_length -= 1
-            else:
-                break
-
-        random_start_index_first_parent = np.random.randint(low=0, high=len(first_parent) - random_length_first_parent + 1)
-        random_start_index_second_parent = np.random.randint(low=0, high=len(second_parent) - second_parent_length + 1)
-
-        child_sequence = first_parent[random_start_index_first_parent:random_length_first_parent] + \
-                         second_parent[random_start_index_second_parent:second_parent_length]
-
-        while True:
-            space = 0
-
-            for group in self.important_groups:
-                if group not in child_sequence:
-                    space += len(group)
-
-            if space == 0:
-                break
-
-            # Hmm, ovdje krešemo samo s početka
-            for _ in range(space):
-                r = np.random.rand()
-
-                if r <= 0.5:
-                    child_sequence = child_sequence[1:]
-                else:
-                    child_sequence = child_sequence[:-1]
-
-        for group in self.important_groups:
-            if group not in child_sequence:
-                r = np.random.rand()
-
-                if r <= 0.5:
-                    first_parent, second_parent = second_parent, first_parent
-
-                i = first_parent.find(group)
-                if i == -1:
-                    i = second_parent.find(group)
-                if i == -1:
-                    i = np.random.randint(low=0, high=len(child_sequence))
-
-                child_sequence = child_sequence[0:i] + group + child_sequence[i:]
-
-        indices = []
-        lens = []
-        for group in self.important_groups:
-            indices.append(child_sequence.find(group))
-            lens.append(len(group))
-
-        p = []
-
-        for i in range(len(child_sequence)):
-            if i in indices:
-                p.append(child_sequence[i:lens[i]]) index of lens aaaaaaa
-                i += lens[i]
-            else:
-                p.append(child_sequence[i])
+                del child_peptide[random_index]
 
 
 
 
-
-
-        # Create a new child object.
-        child = RealNumber(
-            min_value=self.__min_value,
-            max_value=self.__max_value,
-            min_mutation_value=self.__min_mutation_value,
-            max_mutation_value=self.__max_mutation_value,
-            mutation_probability=self._mutation_probability
+        c = ModifiedPeptide(
+            initial_peptide=None,
+            control_mask=None,
+            initial_min_length=0,
+            initial_max_length=0,
+            amino_acids=None,
+            mutation_probability=0.0
         )
 
         # Set child value.
-        child.real_number = (self.real_number + partner.real_number) / 2
+        c.set_peptide(child_peptide)
 
-        return child
+        return c
 
     def mutate(self):
         """Perform mutation, introduce a slight variation.
@@ -214,36 +140,121 @@ class ModifiedPeptide(Subgenome):
         Value is mutated by adding a random number in [self.__min_mutation_value, self.__max_mutation_value)
         range. After the mutation, we must check min and max boundaries.
         """
-        mutation_value = np.random.rand() * \
-            (self.__max_mutation_value - self.__min_mutation_value) + self.__min_mutation_value
 
-        self.real_number = min(max(self.real_number + mutation_value, self.__min_value), self.__max_value)
+
+        # 1. mutacija, dodaj random amino negdje
+
+        # Napraviti da se može i na kraj insertati
+        self.__peptide.insert(
+            np.random.randint(
+                low=0,
+                high=len(self.__peptide)
+            ),
+            self.__amino_acids[np.random.randint(
+                low=0,
+                high=len(self.__amino_acids)
+            )]
+        )
+
+        # 2. mutacija, makni amino random od nekud
+        indices = [i for i in range(len(self.__peptide)) if len(self.__peptide[i]) == 1]
+
+        random_index = indices[np.random.randint(
+            low=0,
+            high=len(indices)
+        )]
+
+        del self.__peptide[random_index]
+
+        # 3. mutacija, zamijeni poredak 2 člana
+        i1 = np.random.randint(
+            low=0,
+            high=len(self.__peptide)
+        )
+
+        i2 = np.random.randint(
+            low=0,
+            high=len(self.__peptide)
+        )
+
+        self.__peptide[i1], self.__peptide[i2] = self.__peptide[i2], self.__peptide[i1]
+
+        # 4. mutacija, promijeni aminokis na nekom mjestu
+        indices = [i for i in range(len(self.__peptide)) if len(self.__peptide[i]) == 1]
+
+        random_index = indices[np.random.randint(
+            low=0,
+            high=len(indices)
+        )]
+
+        random_amino_acid = self.__amino_acids[
+            np.random.randint(
+                low=0,
+                high=len(self.__amino_acids)
+            )]
+
+        self.__peptide[random_index] = random_amino_acid
 
     def extract_important_groups(self, initial_peptide, control_mask):
-        important_groups = []
+        """Extract which important amino groups, and how many of them, should be present in a peptide.
+
+        :param str initial_peptide: Initial amino acid sequence that should be modified.
+        :param List[int, List[int]] control_mask: A mask indicating important amino groups in a peptide. Individual
+        amino acids are indicated by a 1, while groups of amino acids are indicated by a `List`.
+
+        :return:
+            - dict important_groups: A dictionary that contains the important groups and their count.
+            - int length: Sums the length of all the important groups.
+        """
+        important_groups = {}
         index = 0
         length = 0
 
+        # Loop though the members of the `control_mask`.
         for marker in control_mask:
 
+            # If a member is and instance of a `List`, all the member of that list constitute a single important group,
+            # regardless of the values of the members.
             if isinstance(marker, list):
                 group = []
 
+                # Add amino acids to an array.
                 for _ in marker:
                     group.append(initial_peptide[index])
                     index += 1
 
+                # We need to decrement `index` here to ensure `index += 1` at the end of the iteration sets `index` to
+                # the member after the current group.
                 index -= 1
 
-                important_groups.append(''.join(group))
+                # Create a single string from `group` array.
+                group_string = ''.join(group)
+
+                # Add the group to the `important_groups` dictionary.
+                if group_string in important_groups:
+                    important_groups[group_string] += 1
+                else:
+                    important_groups[group_string] = 1
+
+                # Increase the length of the important groups.
                 length += len(group)
 
+            # If a member is not a `List`, and is equal to 1, add it to important groups.
             elif marker == 1:
-                important_groups.append(initial_peptide[index])
+                # A '*' is added to easily differentiate individual amino acids from important groups containing a
+                # single amino acid. This is beneficial during later stages, such as recombination and mutation, while
+                # ensuring the correct amount of important amino groups is present in a peptide.
+                single_amino_group = initial_peptide[index] + "*"
+
+                # Add the group to the `important_groups` dictionary.
+                if single_amino_group in important_groups:
+                    important_groups[single_amino_group] += 1
+                else:
+                    important_groups[single_amino_group] = 1
+
+                # Increase the length of the important groups.
                 length += 1
 
             index += 1
 
         return important_groups, length
-
-
